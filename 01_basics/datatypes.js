4c85cd4b4103939df27ecbd4f7579d1a5499e28e
Suvajit1997 <120557638+Suvajit1997@users.noreply.github.com>
2023-09-25 16:43:56 +0000

    Datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict";
+// alert("hello") this is node js
+
+//primitive types
+
+// string=""
+// number=2 to the power 53
+// BigInt=biggest number than number
+// null=empty value (object type)
+// undefined=value not assigned (undefined type)
+// Boolean=yes/no  ,   true/false
+//symbol=unique value
+
+// console.log(typeof null);
+// console.log(typeof undefined);
+// console.log(typeof "ripun");
+// console.log(typeof 2345);
+
+//nonprimitive types
+
+// object=key:value pair
+// array=holds multiple value
