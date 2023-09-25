85abc1def2e32774a808581ffc93aa14ddcc8373
Suvajit1997 <120557638+Suvajit1997@users.noreply.github.com>
2023-09-25 11:46:14 +0000

    basics

---
+++ 01_basics/scopes.js
@@ -0,0 +1,61 @@
+
+/*  let school="balasore zilla scholl"
+
+function num(){
+    let userName="ripun";
+    var loginId=54321;
+    const website="facebook"
+
+}
+
+// console.log(userName);
+console.log(loginId)
+// console.log(website);
+console.log(school);
+
+*/
+
+
+/* function one(){
+    const userName="ripun";
+    function two(){
+       const website=" facebook";
+       console.log(userName+website)
+    }
+    // console.log(website);
+   
+    two()
+
+}
+// console.log(userName);
+
+one()
+
+*/
+
+/*
+if (true){
+    const userName="ripun"
+    if(userName==="ripun"){
+        const website="facebook"
+        console.log(userName+website);
+    }
+    // console.log(website);
+}
+*/
+
+//function and expression====== Hoisting
+
+/*
+addOne(5)
+function addOne(num){
+    return num+1
+}
+
+
+addTwo(6)
+
+const addTwo=function (num){
+    return num+2
+}
+*/
